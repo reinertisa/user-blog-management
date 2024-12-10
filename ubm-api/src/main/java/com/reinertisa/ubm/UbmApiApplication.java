2685526fdbbb3bc0faf9475085c6777d7a894885
Isa Reinert <reinert.isa@gmail.com>
2024-12-09 22:12:06 -0800

    feat(website): create user blog management project

---
+++ ubm-api/src/main/java/com/reinertisa/ubm/UbmApiApplication.java
@@ -0,0 +1,13 @@
+package com.reinertisa.ubm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbmApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbmApiApplication.class, args);
+    }
+
+}
---
+++ ubm-api/src/test/java/com/reinertisa/ubm/UbmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reinertisa.ubm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbmApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
