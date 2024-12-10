64171032ee978a11cadd0089bee12fc8fe58e195
Isa Reinert <reinert.isa@gmail.com>
2024-12-10 00:39:18 -0800

    feat(website): create blog list component

--- ubm-website/src/components/Home.js
+++ ubm-website/src/components/Home.js
@@ -1,10 +1,17 @@
+import {useState} from "react";
+import BlogList from "./BlogList";
 
 export default function Home() {
 
+    const [blogs, setBlogs] = useState([
+        {title: 'Student university class project', body: 'Bla Bla Bla...', author: 'Isa Reinert', id: 1},
+        {title: 'Blog project management project', body: 'Hey dude', author: 'Sade Miller', id: 2},
+        {title: 'Company tech stack project', body: 'This is professional', author: 'Kristi Reinert', id: 3},
+    ]);
+
     return (
         <div className="home">
-            <h2>Home Page</h2>
+            <BlogList title="All Blogs" blogs={blogs} />
         </div>
     )
-
 }
