80706dc2d08ea99417efd32135e6554444112680
Isa Reinert <reinert.isa@gmail.com>
2024-12-12 16:31:02 -0800

    fix(website): use react router in app component

--- ubm-website/src/App.js
+++ ubm-website/src/App.js
@@ -1,14 +1,23 @@
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
+import Index from "./components/Blog";
+import {BrowserRouter,  Route, Routes} from "react-router";
+import BlogForm from "./components/Blog/Form";
+import BlogDetails from "./components/Blog/Details";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-          <Home />
+      <div className="App">
+          <BrowserRouter>
+              <Navbar/>
+              <div className="content">
+                  <Routes>
+                      <Route path="/" element={<Index />} />
+                      <Route path="/create" element={<BlogForm />} />
+                      <Route path="/blogs/:id" element={<BlogDetails />} />
+                  </Routes>
+              </div>
+          </BrowserRouter>
       </div>
-    </div>
   );
 }
 
