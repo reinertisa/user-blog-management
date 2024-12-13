b3f92811efde12ca18d3c2d9fd4c59939bb85ca2
Isa Reinert <reinert.isa@gmail.com>
2024-12-12 16:29:57 -0800

    fix(website): use Link router component in blog list

---
+++ ubm-website/src/components/Blog/List.js
@@ -0,0 +1,17 @@
+import {Link} from "react-router";
+
+export default function List({title, blogs}) {
+    return (
+        <div className="blog-list">
+            <h2>{title}</h2>
+            {blogs.map(blog => (
+                <div className="blog-preview" key={blog.id}>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{blog.title}</h2>
+                        <p>Written by {blog.author}</p>
+                    </Link>
+                </div>
+            ))}
+        </div>
+    )
+}
