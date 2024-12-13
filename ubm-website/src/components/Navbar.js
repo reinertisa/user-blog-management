d5f4cce9707135f68369d1d0e785c80f3f9a8fd3
Isa Reinert <reinert.isa@gmail.com>
2024-12-12 16:30:12 -0800

    fix(website): use Link router component in navbar component

--- ubm-website/src/components/Navbar.js
+++ ubm-website/src/components/Navbar.js
@@ -1,11 +1,12 @@
+import {Link} from "react-router";
 
 export default function Navbar() {
     return (
         <nav className="navbar">
             <h1>The User Blog Management</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
     )
