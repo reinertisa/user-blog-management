3ccd1307061c9b046e3d4bedb8df7638258fcff7
Isa Reinert <reinert.isa@gmail.com>
2024-12-12 22:57:43 -0800

    refactor(website): rename component name

--- ubm-website/src/components/Blog/index.js
+++ ubm-website/src/components/Blog/index.js
@@ -1,14 +1,14 @@
-import List from "./List";
+import BlogList from "./List";
 import useFetch from "../../hooks/useFetch";
 
-export default function Index() {
+export default function BlogPage() {
     const {data : blogs, loading, error} = useFetch('http://localhost:8080/api/v1/blogs');
 
     return (
         <div className="home">
             {error && <div>{error}</div>}
             {loading && <div>Loading...</div>}
-            {blogs && <List title="All Blogs" blogs={blogs} />}
+            {blogs && <BlogList title="All Blogs" blogs={blogs} />}
         </div>
     )
 }
