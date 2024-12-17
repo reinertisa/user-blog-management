de0aed57589b4e1c3976ebd4ebf71aad7b153a26
Isa Reinert <reinert.isa@gmail.com>
2024-12-16 21:50:55 -0800

    feat(core): update ResourceNotFoundException class

--- ubm-api/src/main/java/com/reinertisa/ubm/exception/ResourceNotFoundException.java
+++ ubm-api/src/main/java/com/reinertisa/ubm/exception/ResourceNotFoundException.java
@@ -1,12 +1,20 @@
 package com.reinertisa.ubm.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Getter
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends Exception {
+public class ResourceNotFoundException extends UbmException {
+    private String fieldName;
 
     public ResourceNotFoundException(String message) {
         super(message);
+    }
+
+    public ResourceNotFoundException(String message, String fieldName) {
+        super(message);
+        this.fieldName = fieldName;
     }
 }
