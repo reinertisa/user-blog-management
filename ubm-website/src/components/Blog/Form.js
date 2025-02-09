337ab545cb52fa37b144dd63e69e662409625948
Isa Reinert <reinert.isa@gmail.com>
2025-02-09 13:51:27 -0800

    fix(website): update blog form

--- ubm-website/src/components/Blog/Form.js
+++ ubm-website/src/components/Blog/Form.js
@@ -1,11 +1,13 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import {useNavigate} from "react-router";
+import useFetch from "../../hooks/useFetch";
 
 export default function BlogForm() {
+    const {data: authorNameOptions, loading, error} = useFetch('http://localhost:8080/api/v1/authors/options');
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
     const [author, setAuthor] = useState('reinert.isa@gmail.com');
-    const [loading, setLoading] = useState(false);
+    const [pending, setPending] = useState(false);
     const navigate = useNavigate();
 
     const handleTitleChange = (e) => setTitle(e.target.value);
@@ -21,49 +23,57 @@ export default function BlogForm() {
             content,
             email: author
         };
-        setLoading(true)
+        setPending(true)
         await fetch('http://localhost:8080/api/v1/blogs', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(blog)
         });
-        setLoading(false);
+        setPending(false);
         navigate('/blogs');
 
     }
 
-    return (
-        <div className="create">
-            <h2>Add a New Blog</h2>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="title">Blog title:</label>
-                <input
-                    id="title"
-                    type="text"
-                    value={title}
-                    required={true}
-                    onChange={handleTitleChange}
-                />
+    let body = <p>Loading....</p>
 
-                <label htmlFor="content">Blog content:</label>
-                <textarea
-                    id="content"
-                    value={content}
-                    onChange={handleContentChange}
-                />
+    if (authorNameOptions?.length > 0) {
+        body = (
+            <div className="create">
+                <h2>Add a New Blog</h2>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="title">Blog title:</label>
+                    <input
+                        id="title"
+                        type="text"
+                        value={title}
+                        required={true}
+                        onChange={handleTitleChange}
+                    />
 
-                <label htmlFor="author">Blog author: </label>
-                <select
-                    id="author"
-                    value={author}
-                    onChange={handleAuthorChange}
-                >
-                    <option value="reinert.isa@gmail.com">Isa Reinert</option>
-                    <option value="sade.miller@gmail.com">Sade Miller</option>
-                    <option value="kristi.reinert@gmail.com">Kristi Reinert</option>
-                </select>
-                <button type="submit" disabled={loading}>{loading ? 'Adding Blog...' : 'Add Blog'}</button>
-            </form>
-        </div>
-    )
+                    <label htmlFor="content">Blog content:</label>
+                    <textarea
+                        id="content"
+                        value={content}
+                        onChange={handleContentChange}
+                    />
+
+                    <label htmlFor="author">Blog author: </label>
+                    <select
+                        id="author"
+                        value={author}
+                        onChange={handleAuthorChange}
+                    >
+                        {authorNameOptions.map(o => (
+                            <option key={`${o.value}--option`} value={o.value}>{o.label}</option>
+                        ))}
+                    </select>
+                    <button type="submit" disabled={pending}>{loading ? 'Adding Blog...' : 'Add Blog'}</button>
+                </form>
+            </div>
+        )
+    } else if (error) {
+        body = <p>Error happened</p>
+    }
+
+    return body;
 }
