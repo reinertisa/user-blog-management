443b5b7d711ae8174fe0d1f485531f59a947080d
Isa Reinert <reinert.isa@gmail.com>
2025-02-09 13:49:54 -0800

    feat(api): add author name options class

---
+++ ubm-api/src/main/java/com/reinertisa/ubm/model/AuthorNameOptions.java
@@ -0,0 +1,35 @@
+package com.reinertisa.ubm.model;
+
+public class AuthorNameOptions {
+    private Long value;
+    private String label;
+
+    public AuthorNameOptions(Long value, String label) {
+        this.value = value;
+        this.label = label;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
+    public void setValue(Long value) {
+        this.value = value;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthorNameOptions{" +
+                "value=" + value +
+                ", label='" + label + '\'' +
+                '}';
+    }
+}
