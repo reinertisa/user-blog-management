940b7e57b56817593e3f29284708c88bbb719244
Isa Reinert <reinert.isa@gmail.com>
2025-02-08 18:22:05 -0800

    fix(api): combine author first name and last name using mapstruct

--- ubm-api/src/main/java/com/reinertisa/ubm/model/BlogMapper.java
+++ ubm-api/src/main/java/com/reinertisa/ubm/model/BlogMapper.java
@@ -2,20 +2,28 @@ package com.reinertisa.ubm.model;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.Named;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring", uses = {AuthorMapper.class})
 public interface BlogMapper {
 
-    @Mapping(source = "id", target = "blogId")
-    @Mapping(source = "author.firstName", target = "authorName")
-    @Mapping(source = "author.email", target = "authorEmail")
+    @Mappings({
+            @Mapping(source = "id", target = "blogId"),
+            @Mapping(source = ".", target = "authorName", qualifiedByName = "combineNames"),
+            @Mapping(source = "author.email", target = "authorEmail")
+    })
     BlogDto toDtoFromEntity(Blog blog);
+
+    @Named("combineNames")
+    default String combineNames(Blog blog) {
+        return blog.getAuthor().getFirstName() + " " + blog.getAuthor().getLastName();
+    }
 
     @Mapping(source = "blogId", target = "id")
     Blog toEntityFromRequest(BlogRequest blogRequest);
 
     List<BlogDto> toDtoListFromEntityList(List<Blog> blogs);
-
 }
