77abb13acf47ff4731f000c0e94c173af52b75de
Isa Reinert <reinert.isa@gmail.com>
2025-03-08 01:47:01 -0800

    fix(api): use Response in AuthorResource

--- ubm-api/src/main/java/com/reinertisa/ubm/resource/AuthorResource.java
+++ ubm-api/src/main/java/com/reinertisa/ubm/resource/AuthorResource.java
@@ -2,9 +2,9 @@ package com.reinertisa.ubm.resource;
 
 
 import com.reinertisa.ubm.domain.Response;
-import com.reinertisa.ubm.dtorequest.dto.AuthorDto;
-import com.reinertisa.ubm.dtorequest.AuthorNameOptions;
-import com.reinertisa.ubm.dtorequest.request.AuthorRequest;
+import com.reinertisa.ubm.dto.Author;
+import com.reinertisa.ubm.dto.AuthorNameOptions;
+import com.reinertisa.ubm.dtorequest.AuthorRequest;
 import com.reinertisa.ubm.service.AuthorService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -14,12 +14,12 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URI;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static com.reinertisa.ubm.utils.RequestUtils.getResponse;
-import static java.util.Collections.emptyMap;
 
 @RestController
 @RequestMapping("/api/v1/authors")
@@ -33,18 +33,26 @@ public class AuthorResource {
     }
 
     @GetMapping("")
-    public ResponseEntity<List<AuthorDto>> getAllAuthors() {
+    public ResponseEntity<Response> getAllAuthors(HttpServletRequest request) {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(authorService.getAllAuthors());
+            List<Author> authors = authorService.getAllAuthors();
+            Map<String, List<Author>> data = new HashMap<>();
+            data.put("authors", authors);
+            return ResponseEntity
+                    .ok()
+                    .body(getResponse(request, data, "All author list", HttpStatus.OK));
         } catch (Exception ex) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), ex);
         }
     }
 
     @GetMapping("/options")
-    public ResponseEntity<List<AuthorNameOptions>> getAllAuthorNames() {
+    public ResponseEntity<Response> getAllAuthorNames(HttpServletRequest request) {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(authorService.getAllAuthorNames());
+            List<AuthorNameOptions> options = authorService.getAllAuthorNames();
+            Map<String, List<AuthorNameOptions>> data = new HashMap<>();
+            data.put("authorNameOptions", options);
+            return ResponseEntity.ok().body(getResponse(request, data, "Author name options", HttpStatus.OK));
         } catch (Exception ex) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), ex);
         }
@@ -53,9 +61,9 @@ public class AuthorResource {
     @PostMapping("")
     public ResponseEntity<Response> createAuthor(@RequestBody @Valid AuthorRequest authorRequest, HttpServletRequest request) {
         try {
-            AuthorDto authorDto = authorService.createAuthor(authorRequest);
-            Map<String, AuthorDto> data = new HashMap<>();
-            data.put("author", authorDto);
+            Author author = authorService.createAuthor(authorRequest);
+            Map<String, Author> data = new HashMap<>();
+            data.put("author", author);
             return ResponseEntity
                     .created(getUri())
                     .body(getResponse(request, data, "Author created.", HttpStatus.CREATED));
@@ -69,8 +77,8 @@ public class AuthorResource {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteAuthor(@PathVariable("id") Long id) {
+    public ResponseEntity<Response> deleteAuthor(@PathVariable("id") Long id, HttpServletRequest request) {
         authorService.deleteAuthor(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+        return ResponseEntity.ok().body(getResponse(request, Collections.emptyMap(),"Author deleted.", HttpStatus.NO_CONTENT));
     }
 }
